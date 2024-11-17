98fcbe374d6aec1d5b9deddb7d073a060aa25bb0
Arnau <arnauet.bayer@gmail.com>
2024-11-17 19:55:19 +0100

    dunno what i did

--- depth_image/build/CMakeFiles/Makefile.cmake
+++ depth_image/build/CMakeFiles/Makefile.cmake
@@ -61,5 +61,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/rs-test.dir/DependInfo.cmake"
+  "CMakeFiles/main.dir/DependInfo.cmake"
   )
