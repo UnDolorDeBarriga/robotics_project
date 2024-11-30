c0a14c3be0c60185d3f661a25f732d093fa0e9f8
Arnau <arnauet.bayer@gmail.com>
2024-11-30 20:46:23 +0100

    Improved the matrichi functions

---
+++ matrix/build/CMakeFiles/build_matrix.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/build_matrix"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/build_matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
